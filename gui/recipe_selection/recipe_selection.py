3b6727090ba0fe7e192b2ab6ec03c3a6e592a834
Jan Sommer <jan-sommer@outlook.com>
2020-09-10 05:58:59 +0200

    First draft implementation of reading recipe json file

--- gui/recipe_selection/recipe_selection.py
+++ gui/recipe_selection/recipe_selection.py
@@ -1,16 +1,17 @@
 from tkinter import Label, filedialog, Button
 
+from recipe.recipe import Recipe
+
 
 class RecipeSelection:
     FILE_TYPES = [
-        ('text files', '*.txt'),
-        ('all files', '*'),
+        ('json files', '*.json')
     ]
 
     def __init__(self, parent):
         self._label_recipe_selection = None
         self._button_recipe_selection = None
-        self._dialog_recipe_selection = None
+        self._selected_recipe_file = None
 
         self.define_widgets(parent)
         self.define_layout()
@@ -24,5 +25,13 @@ class RecipeSelection:
         self._button_recipe_selection.grid(column=0)
 
     def open_selection_dialog(self):
-        self._dialog_recipe_selection = filedialog.askopenfile(initialdir="/", title="Rezept auswählen",
-                                                               filetypes=self.FILE_TYPES)
+        self._selected_recipe_file = filedialog.askopenfilename(initialdir="/", title="Rezept auswählen",
+                                                                filetypes=self.FILE_TYPES)
+        self.read_json_file(self._selected_recipe_file)
+
+    def read_json_file(self, file_path):
+        with open(file_path, "r") as file:
+            json_data = file.read()
+            selected_recipe = Recipe.from_json(json_data)
+            for ingredient in selected_recipe.ingredients:
+                print(ingredient.name)
--- tests/gui/recipe-selection/test_recipe_selection.py
+++ tests/gui/recipe-selection/test_recipe_selection.py
@@ -1,6 +1,10 @@
 import tkinter
+from tkinter import filedialog
+from unittest import mock
+from unittest.mock import patch, mock_open
 
 from gui.recipe_selection.recipe_selection import RecipeSelection
+from recipe.recipe import Recipe
 
 
 def test_recipe_selection_initialization(mocker):
@@ -19,3 +23,29 @@ def test_recipe_selection_initialization(mocker):
     assert recipe_selection
     mock_label_grid.assert_called_once()
     mock_button_grid.assert_called_once()
+
+
+def test_recipe_selection_file_deserialization(mocker):
+    # Given
+    mock_path = "/path/to/json"
+    mock_recipe = Recipe("title", [], [], "preparation")
+
+    mock_parent = None
+    mocker.patch.object(tkinter.Label, "__init__", return_value=None)
+    mocker.patch.object(tkinter.Button, "__init__", return_value=None)
+
+    mocker.patch.object(tkinter.Label, "grid", return_value=None)
+    mocker.patch.object(tkinter.Button, "grid", return_value=None)
+
+    mocker.patch.object(filedialog, "askopenfilename", return_value=mock_path)
+
+    mocker.patch.object(Recipe, "from_json", return_value=mock_recipe)
+
+    with patch("builtins.open", mock_open(read_data="content")) as mock_file_open:
+        recipe_selection = RecipeSelection(mock_parent)
+
+        # When
+        recipe_selection.open_selection_dialog()
+
+        # Then
+        mock_file_open.assert_called_once_with(mock_path, mock.ANY)
