6337ebad14d2ed636b99fac562d09c9d6fab9c07
Jan Sommer <jan-sommer@outlook.com>
2020-09-29 14:38:49 +0200

    Add typings and move member variables into class declaration

--- gui/recipe_form/recipe_form.py
+++ gui/recipe_form/recipe_form.py
@@ -4,21 +4,22 @@ from gui.recipe_form.ingredient_form.ingredient_form import IngredientForm
 from recipe.events.recipe_event import RecipeEvent, RecipeEventType
 from recipe.events.recipe_event_observer import RecipeEventObserver
 from recipe.events.recipe_event_publisher import RecipeEventPublisher
+from recipe.ingredient.ingredient import Ingredient
 from recipe.recipe import Recipe
 
 
 class RecipeForm(Frame, RecipeEventObserver):
+    _label_title: Label = None
+    _entry_title: Entry = None
+    _frame_ingredients: Frame = None
+    _label_ingredients: Label = None
+    _label_preparation: Label = None
+    _text_preparation: Text = None
+    _button_save: Button = None
+
     def __init__(self):
         super().__init__()
-
-        self._label_title = None
-        self._entry_title = None
-        self._frame_ingredients = None
-        self._label_ingredients = None
-        self._label_preparation = None
-        self._text_preparation = None
-        self._button_save = None
-
+        
         self._ingredient_forms = []
 
         RecipeEventPublisher.add(self)
@@ -27,7 +28,7 @@ class RecipeForm(Frame, RecipeEventObserver):
         if event.event_type == RecipeEventType.READ:
             self.set_values(event.payload)
 
-    def define_widgets(self):
+    def define_widgets(self) -> None:
         self._label_title = Label(self, text="Titel")
         self._entry_title = Entry(self)
         self._label_ingredients = Label(self, text="Zutaten")
@@ -36,10 +37,10 @@ class RecipeForm(Frame, RecipeEventObserver):
         self._text_preparation = Text(self)
         self._button_save = Button(self, text="speichern", command=self.save_recipe)
 
-    def configure_layout(self):
+    def configure_layout(self) -> None:
         pass
 
-    def define_layout(self):
+    def define_layout(self) -> None:
         self._label_title.grid(row=0, column=0)
         self._entry_title.grid(row=0, column=1)
         self._label_ingredients.grid(row=1, column=0)
@@ -48,7 +49,7 @@ class RecipeForm(Frame, RecipeEventObserver):
         self._text_preparation.grid(row=4)
         self._button_save.grid(row=5)
 
-    def set_values(self, recipe):
+    def set_values(self, recipe: Recipe) -> None:
         if recipe:
             self.define_widgets()
             self.define_layout()
@@ -60,20 +61,16 @@ class RecipeForm(Frame, RecipeEventObserver):
 
             self._text_preparation.insert(END, recipe.preparation)
 
-    def define_ingredient_form(self, position, ingredient):
+    def define_ingredient_form(self, position: int, ingredient: Ingredient) -> None:
         ingredient_form = IngredientForm(self._frame_ingredients, ingredient)
         ingredient_form.grid(row=position)
 
         self._ingredient_forms.append(ingredient_form)
 
-    def get_recipe(self):
-        ingredients = [form.get_ingredient() for form in self._ingredient_forms]
-        return Recipe(
+    def save_recipe(self) -> None:
+        recipe = Recipe(
             self._entry_title.get(),
             [],
-            ingredients,
-            self._text_preparation.get("1.0", END)
-        )
-
-    def save_recipe(self):
-        RecipeEventPublisher.broadcast(RecipeEvent(RecipeEventType.SAVE, payload=self.get_recipe()))
+            [form.get_ingredient() for form in self._ingredient_forms],
+            self._text_preparation.get("1.0", END))
+        RecipeEventPublisher.broadcast(RecipeEvent(RecipeEventType.SAVE, payload=recipe))
