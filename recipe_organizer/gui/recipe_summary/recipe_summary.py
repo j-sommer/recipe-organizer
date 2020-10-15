8a426ec023e866e9f2b4589762dab7e6b5c2136e
Jan Sommer <jan-sommer@outlook.com>
2020-10-15 18:21:55 +0200

    Add labels to summary and handle mouse events

--- recipe_organizer/gui/recipe_summary/recipe_summary.py
+++ recipe_organizer/gui/recipe_summary/recipe_summary.py
@@ -8,18 +8,41 @@ class RecipeSummary(Frame, WidgetContainer):
     _recipe: Recipe
     _label_title: Label
     _label_title_value: Label
+    _recipe_labels: [Label] = []
 
     def __init__(self, parent, recipe: Recipe):
         Frame.__init__(self, parent, bd=1, relief=RAISED)
         self._recipe = recipe
 
+        self.bind("<Enter>", self.__on_enter)
+        self.bind("<Leave>", self.__on_leave)
+        self.bind("<Button-1>", self.__on_click)
+
         self.define_widgets()
         self.define_layout()
+
+        self.__define_labels()
 
     def define_widgets(self) -> None:
         self._label_title = Label(self, text="Rezept:")
         self._label_title_value = Label(self, text=self._recipe.title)
 
     def define_layout(self) -> None:
-        self._label_title.grid(row=0, column=0)
-        self._label_title_value.grid(row=0, column=1)
+        self._label_title.grid(row=0, column=0, padx=6, pady=6)
+        self._label_title_value.grid(row=0, column=1, padx=6, pady=6)
+
+    def __define_labels(self):
+        for index, label in enumerate(self._recipe.labels):
+            element = Label(self, text=label, bd=1, relief=RAISED)
+            element.grid(row=1, column=index, padx=7, pady=7)
+
+            self._recipe_labels.append(element)
+
+    def __on_enter(self, event) -> None:
+        self.configure(bd=2)
+
+    def __on_leave(self, event) -> None:
+        self.configure(bd=1)
+
+    def __on_click(self, event) -> None:
+        print("clicked")
