681eccf57d6e1c75b5d682d2031a40dd3e42ffb5
Jan Sommer <jan-sommer@outlook.com>
2020-10-15 18:22:44 +0200

    Read recipes from selected source

--- recipe_organizer/gui/recipe_list/recipe_source.py
+++ recipe_organizer/gui/recipe_list/recipe_source.py
@@ -10,8 +10,12 @@ from recipe_organizer.recipe.recipe import Recipe
 
 
 class RecipeSource(Frame, WidgetContainer, EventObserver):
+    _MAX_COLUMN_COUNT = 6
+
     _label_source_directory: Label
     _recipe_summaries: [RecipeSummary] = []
+
+    _row_index = 0
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -25,12 +29,17 @@ class RecipeSource(Frame, WidgetContainer, EventObserver):
         self._label_source_directory = Label(self, text="-")
 
     def define_layout(self) -> None:
-        self._label_source_directory.grid(row=0)
+        self._label_source_directory.grid(row=self.__get_row_index())
 
     def notify(self, event: Event) -> None:
         if event.event_type == EventType.SOURCE_SET:
             self._label_source_directory.configure(text=event.payload.name)
             self.__load_recipes(event.payload)
+
+    def __get_row_index(self) -> int:
+        current_index = self._row_index
+        self._row_index += 1
+        return current_index
 
     def __load_recipes(self, directory: Path):
         recipes: [Recipe] = []
@@ -38,14 +47,24 @@ class RecipeSource(Frame, WidgetContainer, EventObserver):
         for file_path in file_paths:
             with open(file_path, "r", encoding="utf-8") as file:
                 json_data = file.read()
-                recipes.append(Recipe.from_json(json_data))
+                try:
+                    recipe = Recipe.from_json(json_data)
+                except KeyError:
+                    pass
+                else:
+                    recipes.append(recipe)
 
         self.__create_list(recipes)
 
     def __create_list(self, recipes: [Recipe]):
+        current_row_index = self.__get_row_index()
+
         for index, recipe in enumerate(recipes):
+            if index % self._MAX_COLUMN_COUNT == 0:
+                current_row_index = self.__get_row_index()
+
             recipe_summary = RecipeSummary(self, recipe)
-            recipe_summary.grid(row=1, column=index, padx=20)
+            recipe_summary.grid(row=current_row_index, column=index % self._MAX_COLUMN_COUNT, padx=16, pady=10)
             self.columnconfigure(index, minsize=200)
 
             self._recipe_summaries.append(recipe_summary)
